dd01957270c84bb2b33e1b1590e8e9791be05db3
Eric van Wijk <eric@van-wijk.com>
2021-11-24 19:49:32 +0100

    waitSeconds

--- scripts/create_storage_account.ps1
+++ scripts/create_storage_account.ps1
@@ -100,7 +100,7 @@ az storage account blob-service-properties update `
 Open-Firewall -StorageAccountName $Name -ResourceGroupName $ResourceGroup -SubscriptionId $SubscriptionId
 
 if ($Container) {
-    $waitSeconds = 20
+    $waitSeconds = 15
     Write-Host "Waiting $waitSeconds seconds for firewall rules update to reflect..."
     Start-Sleep -Seconds $waitSeconds
 }
