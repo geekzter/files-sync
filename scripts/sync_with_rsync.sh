28584b5d2e149b5bdabef95238cf4bd656636837
Eric van Wijk <eric@van-wijk.com>
2024-12-31 12:38:08 +0100

    Append to target too

--- scripts/sync_with_rsync.sh
+++ scripts/sync_with_rsync.sh
@@ -62,6 +62,7 @@ while IFS=$'\t' read -r source target delete exclude; do
     fi
     rsyncArgs="$rsyncArgs --log-file=${LOG_FILE}"
     targetExpanded="'$(realpath "$target")'"
+    targetExpanded="${targetExpanded%/}/"
     rsyncCommand="rsync $rsyncArgs $sourceExpanded $targetExpanded"
     echo "rsyncCommand: $rsyncCommand"
 
